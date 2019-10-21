1d8d71a8a6d732c4b9161331cf84271620122af8
Luis Arce <luis.arce22@gmail.com>
2019-10-21 10:01:10 -0400

    code: date editor init

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { AgGridReact } from "ag-grid-react";
 import MoodEditor from "./moodEditor.jsx";
 import MoodRenderer from "./moodRenderer.jsx";
+import DateEditor from "./dateEditor.jsx";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
@@ -27,6 +28,12 @@ function App() {
       cellEditor: "moodEditor",
       cellRenderer: "moodRenderer",
       editable: true
+    },
+    {
+      headerName: "Date",
+      field: "date",
+      cellEditor: "dateEditor",
+      editable: true
     }
   ]);
 
@@ -35,25 +42,29 @@ function App() {
       make: "Toyota",
       model: "Celica",
       price: 35000,
-      mood: "Happy"
+      mood: "Happy",
+      date: "2016-02-01"
     },
     {
       make: "Ford",
       model: "Mondeo",
       price: 32000,
-      mood: ""
+      mood: "",
+      date: "2016-02-01"
     },
     {
       make: "Porsche",
       model: "Boxter",
       price: 72000,
-      mood: ""
+      mood: "",
+      date: "2016-02-01"
     }
   ]);
 
   const [frameworkComponents] = React.useState({
     moodRenderer: MoodRenderer,
-    moodEditor: MoodEditor
+    moodEditor: MoodEditor,
+    dateEditor: DateEditor
   });
 
   return (
