2d1016faa5311c7c10fe26f15139b548aa8f8062
Luis Arce <luis.arce22@gmail.com>
2019-10-21 09:17:35 -0400

    code: ag-grid react integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
 function App() {
+  const [columnDefs] = React.useState([
+    {
+      headerName: "Make",
+      field: "make"
+    },
+    {
+      headerName: "Model",
+      field: "model"
+    },
+    {
+      headerName: "Price",
+      field: "price"
+    }
+  ]);
+
+  const [rowData] = React.useState([
+    {
+      make: "Toyota",
+      model: "Celica",
+      price: 35000
+    },
+    {
+      make: "Ford",
+      model: "Mondeo",
+      price: 32000
+    },
+    {
+      make: "Porsche",
+      model: "Boxter",
+      price: 72000
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="ag-theme-balham"
+      style={{
+        height: "300px",
+        width: "800px"
+      }}
+    >
+      <AgGridReact columnDefs={columnDefs} rowData={rowData}></AgGridReact>
     </div>
   );
 }
